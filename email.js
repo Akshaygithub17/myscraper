6f2a781cf79f72fb17b33903a416e7458c870374
Akshaygithub17 <125365519+Akshaygithub17@users.noreply.github.com>
2024-02-27 12:51:08 +0530

    Add files via upload

---
+++ email.js
@@ -0,0 +1,34 @@
+// email.js
+const nodemailer = require('nodemailer');
+
+// Create a transporter using SMTP
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'dhirajspock2121@gmail.com', // Update with your Gmail email
+        pass: 'halh fzkk zjaz lajx' // Update with your Gmail password
+    }
+});
+
+function sendEmail(name, email, message) {
+
+    const mailOptions = {
+
+        // from: 'dhirajspock2121@gmail.com', // Update with your Gmail email
+        from: email,
+        to: 'dhirajspock2121@gmail.com',
+        subject: 'Contact Form Submission',
+        text: `Hello ${name},\n\nThank you for contacting us!\n\nYour message:\n${message}`
+        
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error('Error sending email:', error);
+        } else {
+            console.log('Email sent:', info.response);
+        }
+    });
+}
+
+module.exports = { sendEmail };
